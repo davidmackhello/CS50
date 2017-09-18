4f1112ade2b8d8478b0a2d0b2472336e4bbf76b0
David Machajewski <machajew@gmail.com>
2017-09-18 22:46:36 +0000

    finalized core python function in home

--- git-testing/home/mycode.py
+++ git-testing/home/mycode.py
@@ -2,7 +2,5 @@ def main():
     k = 2 + 2
     l = 5 + 4
     m = k + l
+    print("this is the core function in this code which everyone agrees on")
     return m
-    
-def david():
-    print("David added this line of code after main")
