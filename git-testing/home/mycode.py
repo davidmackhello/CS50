c5666fe9accdd2af1faae2c7bda81a88f4911e3c
David Machajewski <machajew@gmail.com>
2017-09-18 22:52:00 +0000

    david updated core mycode.py with a new fucntion at the end

--- git-testing/home/mycode.py
+++ git-testing/home/mycode.py
@@ -4,3 +4,6 @@ def main():
     m = k + l
     print("this is the core function in this code which everyone agrees on")
     return m
+    
+def david():
+    print("david added a function after main while updating the master branch")
