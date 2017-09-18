272e249f07db017f471a7e106276af0277ef9256
David Machajewski <machajew@gmail.com>
2017-09-18 23:19:58 +0000

    charlie made major changes to mycode.py

--- git-testing/home/mycode.py
+++ git-testing/home/mycode.py
@@ -1,25 +1,10 @@
-def uptop():
-    print("charlie feels this should be the first function")
-
-def charlie():
-    print("charlie added a new function in core python while working on his branch")
-
 def snoopy():
     print("snooooopy")
 
-def main():
-    print("main actually NEEDS to behave like this")
-    print("this is the core function in this code which everyone agrees on")
-    return m
-
-def david():
-    print("david added a function after main while updating the master branch")
-
-def woodstock():
-    print("woodstock")
-
-def downlow():
-    print("charlie feels this could end the file")
+def carnival():
+    print("hello")
+    x = 4 + 5
+    print(x + 5)
 
 def david2():
     print("this function needs to be at the end of the file or else")
