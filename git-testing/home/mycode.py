c5d0f3c403f8a82fd837fca57ff364a005f67a9c
David Machajewski <machajew@gmail.com>
2017-09-18 23:02:56 +0000

    on newbranch, added uptop and downlow functions to mycode cause charlie felt like it

--- git-testing/home/mycode.py
+++ git-testing/home/mycode.py
@@ -1,3 +1,6 @@
+def uptop():
+    print("charlie feels this should be the first function")
+
 def charlie():
     print("charlie added a new function in core python while working on his branch")
 
@@ -7,6 +10,9 @@ def main():
     m = k + l
     print("this is the core function in this code which everyone agrees on")
     return m
-    
+
 def david():
     print("david added a function after main while updating the master branch")
+
+def downlow():
+    print("charlie feels this could end the file")
